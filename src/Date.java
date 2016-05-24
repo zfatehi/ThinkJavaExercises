bb805d134653ffecc07d97d86024fc1d97b10477
zfatehi <zfatehi@gmail.com>
2016-05-24 19:43:45 -0400

    Learning scanner class

---
+++ src/Date.java
@@ -0,0 +1,16 @@
+
+//Exercise 2.2
+
+public class Date {
+	public static void main(String[] args)
+	{
+		String day = "Tuesday";
+		int date = 24;
+		String month = "May";
+		int year = 2016;
+		System.out.println(day + ", " + month + " " + date + ", "+ year);
+		System.out.println(day + " " + date + " " + month + " " + year);
+	
+	}
+}
+
---
+++ src/Time.java
@@ -0,0 +1,22 @@
+
+//Exercise 2.3
+
+public class Time {
+	public static void main(String[] args)
+	{
+		int hour = 12;
+		int minute = 50;
+		int second = 30;
+		
+		System.out.println(hour * 3600 + minute * 60 + second);
+		
+		int hourtwo = 24;
+		int minutetwo = 0;
+		int secondtwo = 0;
+		
+		System.out.println(hourtwo * 3600 + minutetwo * 60 + secondtwo);
+		System.out.println(hourtwo * 3600 + minutetwo * 60 + secondtwo - (hour * 3600 + minute * 60 + second));
+
+		
+	}
+}
