91134c56c9997b6d8c875c9c824ade70306345c0
zfatehi <zfatehi@gmail.com>
2016-05-29 21:58:59 -0400

    Examples up to Chapter 5, Section 2

--- src/Ch4_5_PrintTwice.java
+++ src/Ch4_5_PrintTwice.java
@@ -8,7 +8,16 @@ public class Ch4_5_PrintTwice {
 		System.out.println(s);
 	}
 
+	// So here, there are two variables (boxes) : argument and s. The argument
+	// box only exists inside the following main, and contains the value "Don't
+	// make me say this twice!". When you run the program, you are taking the
+	// value out of the argument box and putting it in the s box so that the
+	// PrintTwice method can run on that value.
+	
 	public static void main(String[] args) {
-		printTwice("Don't make me say this twice!");
+		String argument = "Don't make me say this twice!";
+		printTwice(argument);
+		
+		
 	}
 }
