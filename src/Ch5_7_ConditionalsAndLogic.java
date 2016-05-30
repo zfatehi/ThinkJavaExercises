b3af063383b1ec2b2df849a314b7ef0984014a79
zfatehi <zfatehi@gmail.com>
2016-05-29 22:12:49 -0400

    Error Checking Example

--- src/Ch5_7_ConditionalsAndLogic.java
+++ src/Ch5_7_ConditionalsAndLogic.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 
 public class Ch5_7_ConditionalsAndLogic {
-	
+
 	public static void main(String[] args) {
-		printLogarithm(-1.2);
+		scanDouble();
 	}
-	
+
 	public static void printLogarithm(double x) {
 		if (x <= 0.0) {
 			System.err.println("Error: x must be positive.");
@@ -14,4 +15,16 @@ public class Ch5_7_ConditionalsAndLogic {
 		System.out.println("The log of x is " + result);
 	}
 
+	public static void scanDouble() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+
+		if (!in.hasNextDouble()) {
+			String word = in.next();
+			System.err.println(word + " is not a number");
+			return;
+		}
+		double x = in.nextDouble();
+		printLogarithm(x);
+	}
 }
