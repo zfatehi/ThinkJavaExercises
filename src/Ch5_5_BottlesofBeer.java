71a1ed5a399029337912105dce288decd4c7c5d2
zfatehi <zfatehi@gmail.com>
2016-06-01 21:16:06 -0400

    Working, slightly refined beers

--- src/Ch5_5_BottlesofBeer.java
+++ src/Ch5_5_BottlesofBeer.java
@@ -6,16 +6,26 @@ public class Ch5_5_BottlesofBeer {
 	}
 
 	public static void countdown(int n) {
+		String quantity;
+		String nounstate;
 		if (n == 0) {
-			System.out.println("No bottles of beer on the wall,");
-			System.out.println("no bottles of beer,");
-			System.out.println("ya’ can’t take one down,");
-			System.out.println("ya’ can’t pass it around,");
-			System.out.println("’cause there are no more bottles of beer on the wall!  :( RIP me \n");
+			quantity = "No";
+		} else {
+			quantity = String.valueOf(n);
+		}
+		if (n == 1) {
+			nounstate = "bottle";
+		} else {
+			nounstate = "bottles";
+		}
+		System.out.println(quantity + " " + nounstate + " of beer on the wall,");
+		System.out.println(quantity + " " + nounstate + " of beer,");
+
+		if (n == 0) {
+			System.out.println("ya’ can’t take one down, ya’ can’t pass it around,");
+			System.out.println("’cause there are no more bottles of beer on the wall!\n");
 
 		} else {
-			System.out.println(n + " bottles of beer on the wall,");
-			System.out.println(n + " bottles of beer,");
 			System.out.println("ya’ take one down, ya’ pass it around,");
 			System.out.println(n - 1 + " bottles of beer on the wall.\n");
 
