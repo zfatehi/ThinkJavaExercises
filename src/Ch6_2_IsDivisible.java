5b94ca712e2a914f1696ae2fdc57a66de9491a32
zfatehi <zfatehi@gmail.com>
2016-06-08 15:05:03 -0400

    More Ch.6 Exercises

---
+++ src/Ch6_2_IsDivisible.java
@@ -0,0 +1,18 @@
+
+public class Ch6_2_IsDivisible {
+	public static void main(String[] args) {
+
+		System.out.println(isDivisible(4, 4));
+
+	}
+
+	public static boolean isDivisible(int n, int m) {
+		boolean statement;
+		if (n % m == 0) {
+			statement = true;
+		} else {
+			statement = false;
+		}
+		return statement;
+	}
+}
---
+++ src/Ch6_3_IsTriangle.java
@@ -0,0 +1,19 @@
+
+public class Ch6_3_IsTriangle {
+	public static void main(String[] args) {
+		System.out.println(isTriangle(25, 3, 4));
+	}
+
+	public static boolean isTriangle(int a, int b, int c) {
+		if (a > b + c) {
+			return false;
+		} else if (b > a + c) {
+			return false;
+		} else if (c > a + b) {
+			return false;
+		} else {
+			return true;
+
+		}
+	}
+}
