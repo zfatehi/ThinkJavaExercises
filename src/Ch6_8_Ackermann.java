a034f2d83c0c7c300bce5765af07f9ec09cf6e4b
zfatehi <zfatehi@gmail.com>
2016-06-12 14:06:17 -0400

    All finished, working Ch. 6 exercises.

---
+++ src/Ch6_8_Ackermann.java
@@ -0,0 +1,19 @@
+
+public class Ch6_8_Ackermann {
+	public static void main(String[] args) {
+		System.out.println(ack(2, 5));
+	}
+
+	private static int ack(int m, int n) {
+		if (m == 0) {
+			return n + 1;
+		} else if (m > 0 && n == 0) {
+			return ack(m - 1, 1);
+		} else if (m > 0 && n > 0) {
+			return ack(m - 1, ack(m, n - 1));
+		} else {
+			throw new RuntimeException("Try again with new integers.");
+		}
+	}
+
+}
