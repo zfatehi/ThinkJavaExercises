a42a2f379ef7a249af006dee0af90f4f09cfe9b8
zfatehi <zfatehi@gmail.com>
2016-06-12 13:38:40 -0400

    Working conditions of more Ch 6. Exercises

--- src/Ch6_6_Recurse.java
+++ src/Ch6_6_Recurse.java
@@ -2,17 +2,37 @@
 public class Ch6_6_Recurse {
 
 	public static void main(String[] args) {
+
+		// TEST CASES
+		// Normal cases
 		System.out.println(prod(1, 4));
-		System.out.println(prod(2, 2));
+		System.out.println(prod(3, 6));
+		System.out.println(prod(3, 3));
+
+		// Negative numbers
+		System.out.println(prod(-3, 4));
+		System.out.println(prod(-3, -1));
+		System.out.println(prod(-3, -3));
+
+		// Zeroes
+		System.out.println(prod(0, 4));
+		System.out.println(prod(-1, 0));
+		System.out.println(prod(0, 0));
+
+		// Bad case
+		System.out.println(prod(5, 4));
+
 	}
 
 	public static int prod(int m, int n) {
 		if (m == n) {
 			return n;
-		} else {
+		} else if (m < n) {
 			int recurse = prod(m, n - 1);
 			int result = n * recurse;
 			return result;
+		} else /* if (m < n) */ {
+			throw new RuntimeException("You can't do that! (Beatles reference or nah?)");
 		}
 	}
 
--- src/Ch6_7_OddSum.java
+++ src/Ch6_7_OddSum.java
@@ -1,6 +1,26 @@
 
 public class Ch6_7_OddSum {
 	public static void main(String[] args) {
+		// TEST CASES
 
+		// Normal Case
+		System.out.println(oddSum(5));
+
+		// Bad Cases
+		System.out.println(oddSum(0));
+		System.out.println(oddSum(-1));
+		System.out.println(oddSum(2));
+
+	}
+
+	public static int oddSum(int n) {
+		if (n == 1) {
+			return 1;
+		} else if (n > 1 && n % 2 == 1) {
+			return n + oddSum(n - 2);
+		} else {
+			// return -1; // for testing only
+			throw new RuntimeException("Try a different number.");
+		}
 	}
 }
