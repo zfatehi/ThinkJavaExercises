d102781e24e5cb8dbe37c3f31816d63e76d5ecbb
zfatehi <zfatehi@gmail.com>
2016-06-23 20:14:57 -0400

    Even numbers

---
+++ src/Ch8_SampleArray.java
@@ -0,0 +1,13 @@
+
+public class Ch8_SampleArray {
+
+	public static void main(String[] args) {
+
+		int[] evencounts = new int[10];
+		for (int i = 0; i <= 9; i++) {
+			evencounts[i] = i * 2;
+			System.out.println(evencounts[i]);
+		}
+
+	}
+}
