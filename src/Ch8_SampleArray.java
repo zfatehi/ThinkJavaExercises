28e03b7e54295eb80d82cc03fb31fe731ed9057e
zfatehi <zfatehi@gmail.com>
2016-06-23 20:16:20 -0400

    Two loops

--- src/Ch8_SampleArray.java
+++ src/Ch8_SampleArray.java
@@ -4,8 +4,14 @@ public class Ch8_SampleArray {
 	public static void main(String[] args) {
 
 		int[] evencounts = new int[10];
+
+		// Setting even numbers in the array
 		for (int i = 0; i <= 9; i++) {
 			evencounts[i] = i * 2;
+		}
+
+		// Printing loop
+		for (int i = 0; i <= 9; i++) {
 			System.out.println(evencounts[i]);
 		}
 
