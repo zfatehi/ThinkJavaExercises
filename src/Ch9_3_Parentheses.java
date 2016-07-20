23b1d46e00617ac23aba4e6f03eb33d2faba491d
zfatehi <zfatehi@gmail.com>
2016-07-19 22:53:47 -0400

    Finished editing Ch9_3_Parentheses

---
+++ src/Ch9_3_Parentheses.java
@@ -0,0 +1,32 @@
+
+public class Ch9_3_Parentheses {
+    public static void main(String[] args) {
+        // TEST CASES
+        boolean ans1 = areParenthesesClosed("(20+5(3*4))");
+        System.out.println(ans1);
+        boolean ans2 = areParenthesesClosed("(20+5(3*4)");
+        System.out.println(ans2);
+        boolean ans3 = areParenthesesClosed("20+5(3*4))");
+        System.out.println(ans3);
+        boolean ans4 = areParenthesesClosed("20+5");
+        System.out.println(ans4);
+    }
+
+    private static boolean areParenthesesClosed(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+        }
+        if (count == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
