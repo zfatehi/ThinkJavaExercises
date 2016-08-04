f146d40c81896bec8c5739761d8278ade1b08282
zfatehi <zfatehi@gmail.com>
2016-08-03 20:29:24 -0400

    first two classes to practice constructors created

---
+++ src/exercises/ch11_0_learning_classes/Time.java
@@ -0,0 +1,47 @@
+package exercises.ch11_0_learning_classes;
+
+public class Time {
+
+    private int hour;
+    private int minute;
+    private double second;
+
+    /**
+     * Use this constructor for midnight.
+     */
+    public Time() {
+        this.hour = 0;
+        this.minute = 0;
+        this.second = 0.0;
+    }
+
+    /**
+     * Use this constructor for a specific time.
+     * 
+     * @param hour
+     *            Hours, 0-12
+     * @param minute
+     *            Minutes, 0-59
+     * @param second
+     *            Seconds, 0-59
+     */
+    public Time(int hour, int minute, double second) {
+        if (hour < 0 || hour > 12) {
+            throw new IllegalArgumentException("Hours out of range, " + hour);
+        }
+        this.hour = hour;
+        if (minute < 0 || minute > 59) {
+            throw new IllegalArgumentException("Minutes out of range, " + minute);
+        }
+        this.minute = minute;
+        if (second < 0 || second > 59) {
+            throw new IllegalArgumentException("Seconds out of range, " + second);
+        }
+        this.second = second;
+    }
+
+    public String toString() {
+        return this.hour + ":" + this.minute + ":" + this.second;
+    }
+
+}
---
+++ src/exercises/ch11_0_learning_classes/TimeClient.java
@@ -0,0 +1,10 @@
+package exercises.ch11_0_learning_classes;
+
+public class TimeClient {
+
+    public static void main(String[] args) {
+        Time time = new Time(3, 45, 36);
+        System.out.println(time);
+    }
+
+}
