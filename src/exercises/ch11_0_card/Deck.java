63fd0b41f00bd7f447d665206adb098effe62b16
zfatehi <zfatehi@gmail.com>
2016-09-01 21:20:16 -0400

    Finished shuffle() and toString() functions in the Deck class
    *Will finish other exercises when there is more time

--- src/exercises/ch11_0_card/Deck.java
+++ src/exercises/ch11_0_card/Deck.java
@@ -1,8 +1,11 @@
 package exercises.ch11_0_card;
 
+import java.util.Random;
+
 public class Deck {
 
     private Card[] cards;
+    private Random rand = new Random();
 
     public Deck() {
         this.cards = new Card[52];
@@ -21,4 +24,32 @@ public class Deck {
         }
     }
 
+    public void shuffle() {
+        for (int i = 0; i < this.cards.length; i++) {
+            // choose a random number between i and length - 1
+            int randomcardindex = random(0, this.cards.length - 1);
+            // swap the ith card and the randomly-chosen card
+            swap(this.cards, randomcardindex, i);
+        }
+    }
+
+    private void swap(Card[] cards, int index1, int index2) {
+        Card placeholder = cards[index1];
+        cards[index1] = cards[index2];
+        cards[index2] = placeholder;
+
+    }
+
+    private int random(int min, int max) {
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
+
+    public String toString() {
+        String currentstring = "";
+        for (int i = 0; i < this.cards.length; i++) {
+            currentstring = currentstring + "\n" + this.cards[i];
+        }
+        return currentstring;
+    }
 }
