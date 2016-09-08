dada6376e2ff53672b371e8187404fa6ba7cb6fb
zfatehi <zfatehi@gmail.com>
2016-09-08 12:02:59 -0400

    Successful sysout of shuffled deck, still have to create crazy eights

--- src/exercises/ch11_0_card/DeckClient.java
+++ src/exercises/ch11_0_card/DeckClient.java
@@ -3,14 +3,14 @@ package exercises.ch11_0_card;
 public class DeckClient {
     public static void main(String[] args) {
         Deck deck = new Deck();
-        // System.out.println("Original Deck:");
-        // System.out.println("");
-        // deck.print();
-        // System.out.println("");
-        // deck.shuffle();
-        // System.out.println("Shuffled Deck:");
-        // System.out.println("");
-        // deck.print();
-        System.out.println(deck.toString());
+        System.out.println("Original Deck:");
+        System.out.println("");
+        deck.print();
+        System.out.println("");
+        deck.shuffle();
+        System.out.println("Shuffled Deck:");
+        System.out.println("");
+        deck.print();
+        // System.out.println(deck.toString());
     }
 }
