dbbcfc611ed89f85dd5816c04d07bb2685688d33
zfatehi <zfatehi@gmail.com>
2016-09-08 12:00:49 -0400

    First piece of code run in CS121

---
+++ src/cs121/Demo.java
@@ -0,0 +1,15 @@
+package cs121;
+
+/* This class contains a program that 
+demonstrates addition and printing to the console */
+
+public class Demo {
+    // main method
+    public static void main(String args[]) {
+        // print statements
+        System.out.println("Welcome to 121");
+        System.out.println("3 + 5");
+        System.out.println(3 + 5);
+    }
+
+}
