e265f47ab182424269aa98f751635adeec5d17c3
zfatehi <zfatehi@gmail.com>
2016-10-01 12:38:10 -0400

    StringDiff shorter string length, still have to deal with spaces issue

--- src/cs121projects/StringDiff.java
+++ src/cs121projects/StringDiff.java
@@ -7,6 +7,9 @@ public class StringDiff {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // TODO Deal with spaces issue!!
+
         System.out.print("Enter string one:");
         String inputStr1 = scan.next();
         System.out.print("Enter string two:");
@@ -14,5 +17,21 @@ public class StringDiff {
         System.out.println("String one: " + inputStr1);
         System.out.println("String two: " + inputStr2);
 
+        // Find length of shorter string
+        int inputStr1len = inputStr1.length();
+        int inputStr2len = inputStr2.length();
+        int shortestlen;
+
+        if (inputStr1len < inputStr2len) {
+            shortestlen = inputStr1len;
+        } else {
+            shortestlen = inputStr2len;
+        }
+        System.out.println(shortestlen);
+
+        // Compare letter by letter
+        // If letters are different, print out position (index) and each letter
+        // Every time letter is different, add one to the count
+        // At the very end, print # of differences
     }
 }
