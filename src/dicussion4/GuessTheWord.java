94777d267a0689c737f984e187a761cce9edd79c
zfatehi <zfatehi@gmail.com>
2016-10-01 13:58:47 -0400

    Reporting guesses (letter and count) is working

--- src/dicussion4/GuessTheWord.java
+++ src/dicussion4/GuessTheWord.java
@@ -12,6 +12,7 @@ public class GuessTheWord {
         System.out.println("Welcome to Guess the Word.");
         System.out.println("Here are the vowels and spaces for consonants:");
 
+        // Print a hint for the word, with _ for consonants, and vowels shown
         for (int j = 0; j < wordToGuess.length(); j++) {
             char curChar = wordToGuess.charAt(j);
             if (isVowel(curChar)) {
@@ -20,20 +21,32 @@ public class GuessTheWord {
                 System.out.print('_' + " ");
             }
         }
-
         System.out.println(" ");
+
         // Get the character (consonant) guess from player
-        System.out.println("Enter a character in that word.");
-        String inputChar = scan.next();
+        System.out.println("Enter a character in that word: ");
+        String inputCharStr = scan.next();
+        char inputChar = inputCharStr.charAt(0);
+
         /* Iterate through the word, printing the player's consonant
          if found. Also keep a count of the number of times the consonant 
          is found.  */
-
+        /* For example, if the player types 'h', print
+         * h 1
+         * and if the player types 'p', print
+         * p 0
+         * and if the player types 'l', print
+         * l 2
+         */
         // TODO 3: your code here.
-
-        System.out.println(" ");
+        int count = 0;
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (wordToGuess.charAt(i) == inputChar) {
+                count = count + 1;
+            }
+        }
         // Report findings, prompt to guess the word, capture the player's word
-
+        System.out.println(Character.toString(inputChar) + " " + Integer.toString(count));
         // TODO 4: your code here.
 
         // Check the correctness of the player's guess and report results
@@ -49,6 +62,7 @@ public class GuessTheWord {
         if (curChar == 'a' || curChar == 'e' || curChar == 'i' || curChar == 'o'
                 || curChar == 'u') {
             return true;
+
         } else {
             return false;
         }
