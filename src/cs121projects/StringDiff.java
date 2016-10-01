6ed60f7775437888de97a49fe378d38f97007177
zfatehi <zfatehi@gmail.com>
2016-10-01 13:28:20 -0400

    More readable StringDiff

--- src/cs121projects/StringDiff.java
+++ src/cs121projects/StringDiff.java
@@ -12,10 +12,12 @@ public class StringDiff {
         String inputStr1 = scan.nextLine();
         System.out.print("Enter string two:");
         String inputStr2 = scan.nextLine();
+
         // DEBUG: System.out.println("String one: " + inputStr1);
         // DEBUG: System.out.println("String two: " + inputStr2);
 
         // Find length of shorter string
+
         int inputStr1len = inputStr1.length();
         int inputStr2len = inputStr2.length();
         int shortestlen;
@@ -25,11 +27,13 @@ public class StringDiff {
         } else {
             shortestlen = inputStr2len;
         }
+
         // DEBUG: System.out.println("Shorter string length: " + shortestlen);
 
         // Compare letter by letter
         // If letters are different, print out position (index) and each letter
         // Every time letter is different, add one to the count
+
         int diffcount = 0;
         for (int i = 0; i < shortestlen; i++) {
             char char1cur = inputStr1.charAt(i);
@@ -41,7 +45,9 @@ public class StringDiff {
                                 + " " + Character.toString(char2cur));
             }
         }
+
         // At the very end, print # of differences
+
         System.out.println("Number of differences: " + diffcount);
     }
 }
